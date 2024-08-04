3b2dd58ee9a2d0136bc2f60b68edefcc23b7066c
yukin0510 <a13.ehag@gmail.com>
2024-08-04 20:20:13 +0900

    ログインしてないと見れないように修正

--- equipment/views.py
+++ equipment/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Equipment
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class ListView(ListView):
+class ListView(LoginRequiredMixin, ListView):
     template_name = 'equipment/equip_list.html'
     model = Equipment
     context_object_name = 'equipment'
--- users/views.py
+++ users/views.py
@@ -3,13 +3,14 @@ from django.views import generic
 from .forms import CustomUserCreationForm,CustomUserChangeForm
 from django.views.generic import ListView,TemplateView,UpdateView
 from .models import CustomUser
-from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class IndexView(TemplateView):
     template_name = 'users/index.html'
 
-class UserUpdateView(UpdateView):
-    model = get_user_model()
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = CustomUser
     form_class = CustomUserChangeForm
     template_name = 'users/edit.html'
     success_url = reverse_lazy('users:users')
@@ -22,7 +23,7 @@ class SignUpView(generic.CreateView):
     success_url = reverse_lazy('users:login')  # 登録完了後にリダイレクトするURL
     template_name = 'users/sign_up.html'
 
-class CustomUserListView(ListView):
+class CustomUserListView(LoginRequiredMixin,ListView):
     template_name = 'users/users.html'
     model = CustomUser
     context_object_name = 'users'
