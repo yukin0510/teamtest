619f776f8dffba2a4ce6e0667ed3c6bd7009c5e0
yukin0510 <a13.ehag@gmail.com>
2024-08-07 01:06:27 +0900

    ログイン画面微修正

--- equipment/models.py
+++ equipment/models.py
@@ -15,3 +15,4 @@ class Equipment(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
+
