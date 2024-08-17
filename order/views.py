8c099e76ed0b3cb1041370746cec29729cd61d1a
yukin0510 <a13.ehag@gmail.com>
2024-08-18 01:44:45 +0900

    発注機能(フィルター機能、ページ機能実装)

--- order/views.py
+++ order/views.py
@@ -8,6 +8,17 @@ class OrderListView(LoginRequiredMixin, ListView):
     template_name = 'order/history.html'
     model = Order
     context_object_name = 'orders'
+    paginate_by = 5  # 1ページに表示する発注数
 
     def get_queryset(self):
-        return Order.objects.order_by('-order_date')
+        # すべての注文を取得し、発注日の降順に並べ替える
+        queryset = Order.objects.order_by('-order_date')
+        
+        # GETパラメータからフィルタリングのステータスを取得
+        status_filter = self.request.GET.get('status_filter')
+        
+        # フィルタリングが選択されている場合は、クエリセットをフィルタリング
+        if status_filter:
+            queryset = queryset.filter(approval_status=status_filter)
+        
+        return queryset
